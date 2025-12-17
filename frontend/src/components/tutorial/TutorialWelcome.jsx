2798ae77b7405076591c21eeaec79376b679d663
Matthew Frank <mattafrank2439@gmail.com>
2025-12-17 11:31:28 -0800

    Update logo in tutoral popup

--- frontend/src/components/tutorial/TutorialWelcome.jsx
+++ frontend/src/components/tutorial/TutorialWelcome.jsx
@@ -11,57 +11,14 @@ const TutorialWelcome = ({ modalType, title, content, onAction }) => {
   return (
     <div className="tutorial-welcome-backdrop">
       <div className={`tutorial-welcome-modal ${isCompletion ? 'tutorial-welcome-modal--completion' : ''}`}>
-        {/* Icon */}
-        <div className="tutorial-welcome-icon">
-          {isCompletion ? (
-            <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="32" cy="32" r="30" fill="url(#gradient)" />
-              <path
-                d="M20 32L28 40L44 24"
-                stroke="white"
-                strokeWidth="4"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <defs>
-                <linearGradient id="gradient" x1="0" y1="0" x2="64" y2="64" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#667eea" />
-                  <stop offset="1" stopColor="#9f7aea" />
-                </linearGradient>
-              </defs>
-            </svg>
-          ) : (
-            <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="32" cy="32" r="30" fill="url(#gradient2)" />
-              <path
-                d="M32 18V32L40 40"
-                stroke="white"
-                strokeWidth="4"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M32 18C32 18 24 22 24 32C24 42 32 46 32 46"
-                stroke="white"
-                strokeWidth="3"
-                strokeLinecap="round"
-                opacity="0.5"
-              />
-              <path
-                d="M32 18C32 18 40 22 40 32C40 42 32 46 32 46"
-                stroke="white"
-                strokeWidth="3"
-                strokeLinecap="round"
-                opacity="0.5"
-              />
-              <defs>
-                <linearGradient id="gradient2" x1="0" y1="0" x2="64" y2="64" gradientUnits="userSpaceOnUse">
-                  <stop stopColor="#667eea" />
-                  <stop offset="1" stopColor="#9f7aea" />
-                </linearGradient>
-              </defs>
-            </svg>
-          )}
+        {/* Logo */}
+        <div className="tutorial-welcome-logo">
+          <img
+            src="/InfraSketchLogoTransparent_01_256.png"
+            alt="InfraSketch"
+            width="80"
+            height="80"
+          />
         </div>
 
         {/* Title */}
